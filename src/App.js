55aadf309f73c548753c2b4f3ae6518c86b79c57
Cesar Arroyo <cesardavid_09@hotmail.com>
2020-05-22 17:06:00 -0500

    header-component-styles

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Filter from "./components/Filter";
 import Cart from "./components/Cart";
 import store from "./store";
 import { Provider } from "react-redux";
+import Header from "./components/Header";
 
 class App extends Component {
+    createOrder = (order) => {
+        console.log(order);
+    };
     render() {
         return (
             <Provider store={store}>
                 <div className="grid-container">
-                    <header>
-                        <a href="/">React Shopping Cart</a>
-                    </header>
+                    <Header />
+
                     <main>
                         <div className="content">
                             <div className="main">
@@ -20,7 +23,7 @@ class App extends Component {
                                 <Products />
                             </div>
                             <div className="sidebar">
-                                <Cart />
+                                <Cart createOrder={this.createOrder} />
                             </div>
                         </div>
                     </main>
---
+++ src/components/styledComponents.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const NavBar = styled.header`
+    width: 100%;
+    display: block;
+    margin: 0 auto;
+    background-color: #1a1a1a;
+    /* overflow: hidden; */
+    height: 70px;
+    color: white;
+    align-items: center;
+    img {
+        width: 40px;
+    }
+`;
