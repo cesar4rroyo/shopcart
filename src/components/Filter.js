13c325594776c14f688981c4e8feefc912355fe9
Cesar Arroyo <cesardavid_09@hotmail.com>
2020-05-23 15:55:35 -0500

    adding styles to filter-component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { filterProducts, sortProducts } from "../actions/productActions";
+import "./styles/filter.css";
 
 class Filter extends Component {
     render() {
@@ -9,42 +10,46 @@ class Filter extends Component {
         ) : (
             <div className="filter">
                 <div className="filter-result">
-                    {this.props.filteredProducts.length} Products
+                    <h3>
+                        Se econtraron {this.props.filteredProducts.length}{" "}
+                        productos
+                    </h3>
                 </div>
-                <div className="filter-sort">
-                    Order{" "}
-                    <select
-                        value={this.props.sort}
-                        onChange={(e) =>
-                            this.props.sortProducts(
-                                this.props.filteredProducts,
-                                e.target.value
-                            )
-                        }
-                    >
-                        <option value="latest">Latest</option>
-                        <option value="lowest">Lowest</option>
-                        <option value="highest">Highest</option>
-                    </select>
-                </div>
-                <div className="filter-size">
-                    Filter{" "}
-                    <select
-                        value={this.props.size}
-                        onChange={(e) =>
-                            this.props.filterProducts(
-                                this.props.products,
-                                e.target.value
-                            )
-                        }
-                    >
-                        <option value="">All</option>
-                        <option value="XS">XS</option>
-                        <option value="S">S</option>
-                        <option value="M">M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                    </select>
+                <div className="filter_sort_size">
+                    <div className="filter-sort">
+                        <select
+                            value={this.props.sort}
+                            onChange={(e) =>
+                                this.props.sortProducts(
+                                    this.props.filteredProducts,
+                                    e.target.value
+                                )
+                            }
+                        >
+                            <option value="">Order</option>
+                            <option value="latest">Latest</option>
+                            <option value="lowest">Lowest</option>
+                            <option value="highest">Highest</option>
+                        </select>
+                    </div>
+                    <div className="filter-size">
+                        <select
+                            value={this.props.size}
+                            onChange={(e) =>
+                                this.props.filterProducts(
+                                    this.props.products,
+                                    e.target.value
+                                )
+                            }
+                        >
+                            <option value="">All</option>
+                            <option value="XS">XS</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         );
