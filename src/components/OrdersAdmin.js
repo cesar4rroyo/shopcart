1e55c1d247e5fc542b09b2a678a7fda02d953c44
Cesar Arroyo <cesardavid_09@hotmail.com>
2020-06-09 21:54:45 -0500

    adding admin without login

---
+++ src/components/OrdersAdmin.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchOrder } from "../actions/orderActions";
+
+class OrdersAdmin extends Component {
+    componentDidMount() {
+        this.props.fetchOrder();
+    }
+    render() {
+        const { orders } = this.props;
+        return !orders ? (
+            <div>Loading</div>
+        ) : (
+            <div className="orders">
+                <h2>Orders</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>DATE</th>
+                            <th>TOTAL</th>
+                            <th>NAME</th>
+                            <th>EMAIL</th>
+                            <th>ADDRESS</th>
+                            <th>ITEMS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order, index) => (
+                            <tr key={index}>
+                                <td>{order._id}</td>
+                                <td>{order.createdAt}</td>
+                                <td>{order.total}</td>
+                                <td>{order.name}</td>
+                                <td>{order.email}</td>
+                                <td>{order.address}</td>
+                                <td>
+                                    {order.cartItems.map((item, index) => (
+                                        <div key={index}>
+                                            {item.count} {" x "} {item.title}
+                                        </div>
+                                    ))}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    (state) => ({
+        orders: state.order.orders,
+    }),
+    { fetchOrder }
+)(OrdersAdmin);
